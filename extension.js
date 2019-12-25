1373cd9e35a0ac0f090f7dda4e622ac3d6e12c37
Young Jae Sim <hanul@hanul.me>
2019-12-25 17:29:31 +0900

    1차 구현 완료

---
+++ extension.js
@@ -0,0 +1,58 @@
+const VSCode = require('vscode');
+const FS = require('fs');
+const Path = require('path');
+
+exports.activate = (context) => {
+	context.subscriptions.push(VSCode.commands.registerCommand('sky-csv.edit', () => {
+
+		let filePath = VSCode.window.activeTextEditor.document.fileName;
+		let fileName = Path.basename(filePath);
+
+		let webView = VSCode.window.createWebviewPanel(
+			'Sky CSV',
+			fileName,
+			VSCode.ViewColumn.One,
+			{
+				enableScripts: true
+			}
+		).webview;
+
+		let editor = VSCode.window.activeTextEditor;
+
+		webView.onDidReceiveMessage((message) => {
+
+			if (message.command === 'loadData') {
+				webView.postMessage({
+					command: 'loadData',
+					content: editor.document.getText()
+				});
+			}
+
+			else if (message.command === 'save') {
+				FS.writeFileSync(filePath, message.content, 'utf8');
+				VSCode.window.showInformationMessage(fileName + ' saved.');
+			}
+		});
+
+		let resource = (path) => {
+			return 'vscode-resource:' + Path.join(context.extensionPath, path).replace(/\\/g, '/');
+		};
+
+		webView.html = `<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge, chrome=1">
+		<link rel="stylesheet" media="screen" href="${resource('handsontable/handsontable.full.min.css')}">
+		<link rel="stylesheet" media="screen" href="${resource('base.css')}">
+	</head>
+	<body>
+		<script src="${resource('handsontable/handsontable.full.min.js')}"></script>
+		<script src="${resource('handsontable/BROWSER.js')}"></script>
+		<script src="${resource('papaparse.js')}"></script>
+		<script src="${resource('index.js')}"></script>
+	</body>
+</html>`;
+	}));
+};
---
+++ index.js
@@ -0,0 +1,119 @@
+'use strict';
+
+const VSCode = acquireVsCodeApi();
+
+INIT_OBJECTS();
+
+let isControlMode;
+let searchForm;
+
+EVENT('keydown', (e) => {
+
+    if (isControlMode === true) {
+
+        let key = e.getKey().toLowerCase();
+
+        // 현재 탭 종료
+        if (key === 'w' || key === 'q') {
+            location.href = 'about:blank';
+        }
+
+        // 데이터 저장
+        if (key === 's') {
+
+            let lines = __PAPA.unparse({
+                data: hot.getData()
+            });
+
+            let content = '';
+            lines.split('\n').forEach((line) => {
+                line = line.trim();
+                for (let i = line.length - 1; i >= 0; i -= 1) {
+                    if (line[i] !== ',') {
+                        line = line.substring(0, i + 1);
+                        break;
+                    }
+                    if (i === 0) {
+                        line = '';
+                    }
+                }
+                content += line + '\n';
+            });
+
+            VSCode.postMessage({
+                command: 'save',
+                content: content.trim()
+            })
+            e.stopDefault();
+        }
+
+        // 검색
+        if (key === 'f') {
+
+            let input;
+            searchForm = FORM({
+                style: {
+                    position: 'fixed',
+                    right: 0,
+                    top: 0,
+                    zIndex: 999999,
+                    padding: 5,
+                    borderRadius: 3,
+                    backgroundColor: '#eee'
+                },
+                c: input = INPUT({
+                    style: {
+                        width: 300
+                    },
+                    name: 'query',
+                    placeholder: 'Find'
+                }),
+                on: {
+                    keyup: () => {
+                        hot.getPlugin('search').query(input.getValue());
+                        hot.render();
+                    }
+                }
+            }).appendTo(BODY);
+
+            hot.deselectCell();
+            input.focus();
+        }
+    }
+
+    if (e.getKey() === 'Control') {
+        isControlMode = true;
+    }
+
+    if (e.getKey() === 'Escape') {
+        searchForm.remove();
+        searchForm = undefined;
+    }
+});
+
+EVENT('keyup', (e) => {
+    if (e.getKey() === 'Control') {
+        isControlMode = false;
+    }
+});
+
+global.hot = new Handsontable(document.body, {
+    minRows: 1000,
+    minCols: 20,
+    rowHeaders: true,
+    colHeaders: true,
+    colWidths: 280,
+    manualColumnResize: true,
+    manualRowResize: true,
+    search: true
+});
+
+window.addEventListener('message', (event) => {
+    if (event.data.command === 'loadData') {
+        hot.loadData(__PAPA.parse(event.data.content).data);
+    }
+});
+
+VSCode.postMessage({
+    command: 'loadData'
+});
